8ff73182b66a48a34b4d72b56bdae1b63080e4fa
watamario15 <watamario15@outlook.jp>
2021-11-01 18:18:45 +0900

    Initial Commit

---
+++ G6team/G6LeaderRobot.java
@@ -0,0 +1,23 @@
+package G6team;
+import robocode.*;
+import java.awt.Color;
+
+public class G6LeaderRobot extends TeamRobot
+{
+	public void run() {	// G6LeaderRobot's default behavior
+		setColors(Color.gray, Color.yellow, Color.yellow); // body, gun, radar
+		setTurnRadarRightRadians(100000); // Always search for enemies in all direction
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) { // What to do when you see another robot
+		
+	}
+
+	public void onHitByBullet(HitByBulletEvent e) { //  What to do when you're hit by a bullet
+		
+	}
+	
+	public void onHitWall(HitWallEvent e) { // What to do when you hit a wall
+		
+	}	
+}
---
+++ G6team/G6SubRobot2.java
@@ -0,0 +1,23 @@
+package G6team;
+import robocode.*;
+import java.awt.Color;
+
+public class G6SubRobot2 extends TeamRobot
+{
+	public void run() {	// G6SubRobot2's default behavior
+		setColors(Color.gray, Color.yellow, Color.yellow); // body, gun, radar
+		setTurnRadarRightRadians(100000); // Always search for enemies in all direction
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) { // What to do when you see another robot
+		
+	}
+
+	public void onHitByBullet(HitByBulletEvent e) { //  What to do when you're hit by a bullet
+		
+	}
+	
+	public void onHitWall(HitWallEvent e) { // What to do when you hit a wall
+		
+	}	
+}
