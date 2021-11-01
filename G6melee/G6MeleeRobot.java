de165f2b521611b7549d8e3e8c9ec2424c3099c4
watamario15 <watamario15@outlook.jp>
2021-11-01 18:23:10 +0900

    Fix spaces

--- G6melee/G6MeleeRobot.java
+++ G6melee/G6MeleeRobot.java
@@ -4,20 +4,20 @@ import java.awt.Color;
 
 public class G6MeleeRobot extends AdvancedRobot
 {
-	public void run() {	// G6MeleeRobot's default behavior
-		setColors(Color.gray, Color.yellow, Color.yellow); // body, gun, radar
-		setTurnRadarRightRadians(100000); // Always search for enemies in all direction
-	}
+    public void run() { // G6MeleeRobot's default behavior
+        setColors(Color.gray, Color.yellow, Color.yellow); // body, gun, radar
+        setTurnRadarRightRadians(100000); // Always search for enemies in all direction
+    }
 
-	public void onScannedRobot(ScannedRobotEvent e) { // What to do when you see another robot
-		
-	}
+    public void onScannedRobot(ScannedRobotEvent e) { // What to do when you see another robot
+        
+    }
 
-	public void onHitByBullet(HitByBulletEvent e) { //  What to do when you're hit by a bullet
-		
-	}
-	
-	public void onHitWall(HitWallEvent e) { // What to do when you hit a wall
-		
-	}	
+    public void onHitByBullet(HitByBulletEvent e) { //  What to do when you're hit by a bullet
+        
+    }
+    
+    public void onHitWall(HitWallEvent e) { // What to do when you hit a wall
+        
+    }
 }
