358d3a9e70d98d858f845991140a80278481660a
tth-test <yhtsto.2702@outlook.jp>
2021-11-11 00:53:18 +0900

    changed to add random movement (and simple wall-avoidance) system

--- G6team/G6LeaderRobot.java
+++ G6team/G6LeaderRobot.java
@@ -2,12 +2,36 @@ package G6team;
 import robocode.*;
 import java.awt.Color;
 
-public class G6LeaderRobot extends TeamRobot
-{
-    public void run() { // G6LeaderRobot's default behavior
-        setColors(Color.gray, Color.yellow, Color.yellow); // body, gun, radar
-        setTurnRadarRightRadians(100000); // Always search for enemies in all direction
+import java.util.Random;//added to use random values
+
+public void run() { // G6LeaderRobot's default behavior
+    setColors(Color.gray, Color.yellow, Color.yellow); // body, gun, radar
+    setTurnRadarRightRadians(100000); // Always search for enemies in all direction
+    
+    //random movement
+    Random rnd = new Random();
+    double x,y,height = getBattleFieldHeight();
+    double distance = height/3;
+    
+    while(true) {
+    	if(rnd.nextBoolean()) {
+    		turnRight(30+rnd.nextDouble()*120);
+    	}else {
+    		turnLeft(30+rnd.nextDouble()*120);
+    	}
+    	setAhead(distance);
+    	execute();
+    	while(getVelocity()>0) {
+    		x = getX();
+    		y = getY();
+    		execute();
+			if(x<50||x>height-50||y<50||y>height-50){
+				back(distance-getDistanceRemaining());
+			}
+    	}
     }
+    //end of random movement
+}
 
     public void onScannedRobot(ScannedRobotEvent e) { // What to do when you see another robot
         
--- G6team/G6SubRobot1.java
+++ G6team/G6SubRobot1.java
@@ -2,11 +2,37 @@ package G6team;
 import robocode.*;
 import java.awt.Color;
 
+import java.util.Random;//added to use random values
+
 public class G6SubRobot1 extends TeamRobot
 {
     public void run() { // G6SubRobot1's default behavior
         setColors(Color.gray, Color.yellow, Color.yellow); // body, gun, radar
         setTurnRadarRightRadians(100000); // Always search for enemies in all direction
+        
+        //random movement
+        Random rnd = new Random();
+        double x,y,height = getBattleFieldHeight();
+        double distance = height/3;
+        
+        while(true) {
+        	if(rnd.nextBoolean()) {
+        		turnRight(30+rnd.nextDouble()*120);
+        	}else {
+        		turnLeft(30+rnd.nextDouble()*120);
+        	}
+        	setAhead(distance);
+        	execute();
+        	while(getVelocity()>0) {
+        		x = getX();
+        		y = getY();
+        		execute();
+				if(x<50||x>height-50||y<50||y>height-50){
+					back(distance-getDistanceRemaining());
+				}
+        	}
+        }
+        //end of random movement
     }
 
     public void onScannedRobot(ScannedRobotEvent e) { // What to do when you see another robot
