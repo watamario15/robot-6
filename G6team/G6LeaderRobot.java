4995c5396c14b994d967cc633e759c104176d329
littlegirl0820 <littlegirl112@outlook.jp>
2021-11-12 16:04:38 +0900

    HOGE

--- G6team/G6LeaderRobot.java
+++ G6team/G6LeaderRobot.java
@@ -1,4 +1,5 @@
 package G6team;
+
 import robocode.*;
 import java.awt.Color;
 
@@ -8,41 +9,41 @@ public class G6LeaderRobot extends TeamRobot {
     public void run() { // G6LeaderRobot's default behavior
         setColors(Color.gray, Color.yellow, Color.yellow); // body, gun, radar
         setTurnRadarRightRadians(100000); // Always search for enemies in all direction
-        
-        //random movement
+
+        // random movement
         Random rnd = new Random();
-        double x,y,height = getBattleFieldHeight();
-        double distance = height/3;
-        
-        while(true) {
-            if(rnd.nextBoolean()) {
-                turnRight(30+rnd.nextDouble()*120);
-            }else {
-                turnLeft(30+rnd.nextDouble()*120);
+        double x, y, height = getBattleFieldHeight();
+        double distance = height / 3;
+
+        while (true) {
+            if (rnd.nextBoolean()) {
+                turnRight(30 + rnd.nextDouble() * 120);
+            } else {
+                turnLeft(30 + rnd.nextDouble() * 120);
             }
             setAhead(distance);
             execute();
-            while(getVelocity()>0) {
+            while (getVelocity() > 0) {
                 x = getX();
                 y = getY();
                 execute();
-                if(x<50||x>height-50||y<50||y>height-50){
-                    back(distance-getDistanceRemaining());
+                if (x < 50 || x > height - 50 || y < 50 || y > height - 50) {
+                    back(distance - getDistanceRemaining());
                 }
             }
         }
-        //end of random movement
+        // end of random movement
     }
 
     public void onScannedRobot(ScannedRobotEvent e) { // What to do when you see another robot
-        
+
     }
 
-    public void onHitByBullet(HitByBulletEvent e) { //  What to do when you're hit by a bullet
-        
+    public void onHitByBullet(HitByBulletEvent e) { // What to do when you're hit by a bullet
+
     }
-    
+
     public void onHitWall(HitWallEvent e) { // What to do when you hit a wall
-        
+
     }
 }
