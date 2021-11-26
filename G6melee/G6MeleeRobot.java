7c4fbb9f5937f0be596803141d62fa1bbf4719d3
tth-test <yhtsto.2702@outlook.jp>
2021-11-26 15:43:19 +0900

    changed the random movement.

--- G6melee/G6MeleeRobot.java
+++ G6melee/G6MeleeRobot.java
@@ -1,69 +1,27 @@
 package G6melee;
 
 import robocode.*;
+import java.util.Random;
 import java.awt.Color;
+import robocode.util.Utils;
+import java.awt.geom.Rectangle2D;
+
 
 import java.util.Random;//added to use random values
 
 public class G6MeleeRobot extends AdvancedRobot {
-    double direction = 100000;
 
     public void run() { // G6MeleeRobot's default behavior
         setColors(Color.gray, Color.yellow, Color.yellow); // body, gun, radar
         setTurnRadarRightRadians(100000); // Always search for enemies in all direction
 
-        // random movement
-        Random rnd = new Random();
-        double x, y, height = getBattleFieldHeight();
-        double distance = height / 3;
-
-        while (true) {
-            if (rnd.nextBoolean()) {
-                turnRight(30 + rnd.nextDouble() * 120);
-            } else {
-                turnLeft(30 + rnd.nextDouble() * 120);
-            }
-            setAhead(distance);
-            execute();
-            while (getVelocity() > 0) {
-                x = getX();
-                y = getY();
-                execute();
-                if (x < 50 || x > height - 50 || y < 50 || y > height - 50) {
-                    back(distance - getDistanceRemaining());
-                }
-            }
+        while(true) {
+        	randomMovement();
         }
-        // end of random movement
     }
 
     public void onScannedRobot(ScannedRobotEvent e) { // What to do when you see another robot
-        int okDistance = 200;
-        if (getDistance() <= okDistance) {
-            // ButtHead no nanigashika wo suru
-            double absoluteBearing;
-            double bearing;
-            double lead;
 
-            // gun
-            // normalRelativeAngle:角度の正規化([-pi,pi)にする)
-            // getHeadingRadians:向きをラジアンで得る．
-            // e.getBearingRadians:敵の相対位置(自機からの)をラジアンで得る．
-            // getVelocity:速度を得る．
-            setTurnGunRightRadians(Utils
-                    .normalRelativeAngle((absoluteBearing = getHeadingRadians() + (bearing = e.getBearingRadians()))
-                            + (lead = Math
-                                    .asin(e.getVelocity() / 14 * Math.sin(e.getHeadingRadians() - absoluteBearing)))
-                            - getGunHeadingRadians()));
-            setTurnGunRightRadians(Utils.normalRelativeAngle(getHeadingRadians() + e.getBearingRadians()
-                    + Math.asin(e.getVelocity() / 14 * Math.sin(e.getHeadingRadians() - absoluteBearing))
-                    - getGunHeadingRadians()));
-            // orbiting
-            setAhead(direction);
-            setTurnRightRadians(
-                    Math.cos(bearing - ((absoluteBearing = e.getDistance()) - 160) * (direction / 35000000)));
-            setFire(2);
-        }
     }
 
     public void onHitByBullet(HitByBulletEvent e) { // What to do when you're hit by a bullet
@@ -71,6 +29,39 @@ public class G6MeleeRobot extends AdvancedRobot {
     }
 
     public void onHitWall(HitWallEvent e) { // What to do when you hit a wall
+
+    }
+    
+    private void randomMovement() {
+		Random rnd = new Random();
+		double direction = 1;
+		if(rnd.nextBoolean()){
+			direction *= -1;
+		}
+		
+		Rectangle2D fieldRect = new Rectangle2D.Double(80, 80, getBattleFieldWidth()-160, getBattleFieldHeight()-160); // make a square in the field. it is the safety area.
+		setAhead(100000); // always go ahead
+		setTurnRight(direction*(30+rnd.nextDouble()*120)); // 
+		double startTime = getTime();
+		while(/*getTime()-startTime<10.0*/getTurnRemaining()!=0){
+			double goalDirection = getHeadingRadians();
+			while (!fieldRect.contains(getX()+Math.sin(goalDirection)*150, getY()+
+					Math.cos(goalDirection)*150))
+			{
+				goalDirection += direction*.1;
+			}
+			double turn =
+				robocode.util.Utils.normalRelativeAngle(goalDirection-getHeadingRadians());
+			if (Math.abs(turn) > Math.PI/2)
+			{
+				turn = robocode.util.Utils.normalRelativeAngle(turn + Math.PI);
+				setBack(100);
+			}
+			else
+				//setAhead(100);
+			if(turn != 0)setTurnRightRadians(turn);
+			execute();
+		}
 
     }
 }
