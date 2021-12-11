181c438b9a4f59abd9cf5c920d96cf532945b3ad
myname <7.15sugiyama74.6@gmail.com>
2021-12-11 21:17:41 +0900

    test team

--- G6common/Message.java
+++ G6common/Message.java
@@ -2,8 +2,22 @@ package G6common;
 
 import robocode.*;
 import java.io.Serializable;
+import java.util.*;
 
 public class Message implements Serializable {
-    int x, y, targetX, targetY;
     String targetName;
+    double x,y,targetX,targetY; 
+
+    public Message() {
+        System.out.println("PBL yametai");
+    }
+
+    public Message(String targetName2) {
+        targetName = targetName2;
+    }
+
+    public String getTargetName() {
+        return targetName;
+    }
+
 }
